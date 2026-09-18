ca9a0271c8a85f19804732a9916154af8d138e63
AS <auriza@gmail.com>
2026-09-18 15:08:47 +0700

    test lagi

--- script.js
+++ script.js
@@ -5,11 +5,9 @@ grist.onRecords(table => {
 
 grist.onRecord(function(record) {
   //document.getElementById('title').innerHTML = `<a href="${record.link}">${record.A}</a>`;
-  //document.getElementById('title').innerText = record.link;
+  document.getElementById('title').innerText = record.link;
 
-  console.log("Selected record:", record);
-  console.log("record.link:", record.link);
-  document.getElementById('title').innerText = record.link ?? "No link";
+  
 });
 
 //console.log("grist.ready() called");
