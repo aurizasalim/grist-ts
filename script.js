05a5e7c05b8bda0851c59c6e13996c82ae451bcf
AS <auriza@gmail.com>
2026-09-18 15:04:30 +0700

    test lagi

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-grist.ready({ requiredAccess: 'none' });
+grist.ready({ requiredAccess: 'read table' });
 grist.onRecords(table => {
 
 });
-grist.onRecord(function(record) {
-  document.getElementById('title').innerHTML = `<a href="${record.link}">${record.A}</a>`;
 
+grist.onRecord(function(record) {
+  //document.getElementById('title').innerHTML = `<a href="${record.link}">${record.A}</a>`;
+  //document.getElementById('title').innerText = record.link;
+
+  console.log("Selected record:", record);
+  console.log("record.link:", record.link);
+  document.getElementById('title').innerText = record.link ?? "No link";
 });
+
+//console.log("grist.ready() called");
+//document.getElementById('title').innerText = "test pada script diluar onRecord";
+//Sudah positif keload scriptnya
